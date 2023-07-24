0a9119f424e0e0e5f2c3e1bbbf5947fbc34aeabf
Sahil khan <116485673+SahilKhan44@users.noreply.github.com>
2023-07-24 12:43:38 +0530

    added

---
+++ src/components/Projects.jsx
@@ -0,0 +1,121 @@
+import { Image, Text } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { animate, useMotionValue } from "framer-motion";
+
+import { motion } from "framer-motion-3d";
+import { atom, useAtom } from "jotai";
+import { useEffect, useRef } from "react";
+
+export const projects = [
+  {
+    title: "Netflix Clone",
+    url: " https://sahilkhan44.github.io/Netflix/",
+    image: "projects/clone.jpg",
+    description: "Netflix Clone using HTML | CSS | JS",
+  },
+  {
+    title: "Music WebApp",
+    url: "https://sahilkhan44.github.io/Music-App/",
+    image: "projects/music.jpg",
+    description: "Basic Music WebApp using HTML | CSS | JS",
+  },
+  {
+    title: "Real-Time ChatApp",
+    url: "https://sahilkhan44.github.io/CodeClause-Real-Time-Chat-App/",
+    image: "projects/download.png",
+    description: "Real-Time ChatApp using NodeJs | Socket.io",
+  },
+
+  {
+    title: "Resume-Builder",
+    url: "https://sahilkhan44.github.io/Resume-Builder/",
+    image: "projects/Resume.jpg",
+    description: "Resume-Builder-Using-Vanilla-Javascript",
+  },
+  {
+    title: "Temprature Converter",
+    url: "https://sahilkhan44.github.io/Temperature-converter/",
+    image: "projects/temp.png",
+    description: "Basic Temprature coversion App using HTML | CSS | JS",
+  },
+ 
+];
+
+const Project = (props) => {
+  const { project, highlighted } = props;
+
+  const background = useRef();
+  const bgOpacity = useMotionValue(0.4);
+
+  useEffect(() => {
+    animate(bgOpacity, highlighted ? 0.7 : 0.4);
+  }, [highlighted]);
+
+  useFrame(() => {
+    background.current.material.opacity = bgOpacity.get();
+  });
+
+  return (
+    <group {...props}>
+      <mesh
+        position-z={-0.001}
+        onClick={() => window.open(project.url, "_blank")}
+        ref={background}
+      >
+        <planeGeometry args={[2.2, 2]} />
+        <meshBasicMaterial color="black" transparent opacity={0.4} />
+      </mesh>
+      <Image
+        scale={[2, 1.2, 1]}
+        url={project.image}
+        toneMapped={false}
+        position-y={0.3}
+      />
+      <Text
+        maxWidth={2}
+        anchorX={"left"}
+        anchorY={"top"}
+        fontSize={0.2}
+        position={[-1, -0.4, 0]}
+      >
+        {project.title.toUpperCase()}
+      </Text>
+      <Text
+        maxWidth={2}
+        anchorX="left"
+        anchorY="top"
+        fontSize={0.1}
+        position={[-1, -0.6, 0]}
+      >
+        {project.description}
+      </Text>
+    </group>
+  );
+};
+
+export const currentProjectAtom = atom(Math.floor(projects.length / 2));
+
+export const Projects = () => {
+  const { viewport } = useThree();
+  const [currentProject] = useAtom(currentProjectAtom);
+
+  return (
+    <group position-y={-viewport.height * 2 + 1}>
+      {projects.map((project, index) => (
+        <motion.group
+          key={"project_" + index}
+          position={[index * 2.5, 0, -3]}
+          animate={{
+            x: 0 + (index - currentProject) * 2.5,
+            y: currentProject === index ? 0 : -0.1,
+            z: currentProject === index ? -2 : -3,
+            rotateX: currentProject === index ? 0 : -Math.PI / 3,
+            rotateZ: currentProject === index ? 0 : -0.1 * Math.PI,
+          }}
+        >
+          <Project project={project} highlighted={index === currentProject} />
+        </motion.group>
+      ))}
+    </group>
+  );
+};
